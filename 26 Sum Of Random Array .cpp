bb95af2abc6f1b3d725aa960be1cf8d0e335cf79
AboubakerDev <bouboualjzayr666@gmail.com>
2026-01-20 16:18:39 +0100

    Add header comments to 26 Sum Of Random Array.cpp
    Add header comments for exercise details.

---
+++ 26 Sum Of Random Array .cpp
@@ -0,0 +1,58 @@
+/*
+Exercise 26
+Programming Advice - Course 5
+Solved in October 2024
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int ReadPositivNumberLess100(string Message) {
+	int n;
+	do {
+		cout << Message;
+		cin >> n;
+	} while (n <= 0 || n > 100);
+	return n;
+}
+
+int Random(int From, int To) {
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int Array[], int& ArrayLength) {
+	ArrayLength= ReadPositivNumberLess100("Please enter a number of elements\t");
+
+	for (int i =0; i < ArrayLength; i++)
+	{
+		Array[i] = Random(1,100);
+	}
+}
+
+void PrintArray(int ArrayElement[], int ArrayLength) {
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		cout << ArrayElement[i] << " ";
+	}
+}
+
+int SumArray(int ArrayElement[], int ArrayLength) {
+	int Sum = 0;
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		Sum += ArrayElement[i];
+	}
+	return Sum;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Array[100];
+	int ArrauLength;
+	FillArrayWithRandomNumbers(Array, ArrauLength);
+	cout << "Array elemments are : \t";
+	PrintArray(Array, ArrauLength);
+	cout << "\n Sum of all numbers is: \t" << SumArray(Array, ArrauLength);
+	return 0;
+}
