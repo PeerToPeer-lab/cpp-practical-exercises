6e2ff630bc24f9dda51ccde0739e53e3ab465b33
AboubakerDev <bouboualjzayr666@gmail.com>
2026-01-25 14:43:19 +0100

    Create 07 Reveres Number .cpp

---
+++ Exercises/07 Reveres Number .cpp
@@ -0,0 +1,33 @@
+/*
+Exercise 07
+Programming Advice - Course 5
+Solved in October 2024
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int ReadPositivNumber(string Message) {
+	int n;
+	do {
+		cout << Message;
+		cin >> n;
+	} while (n <= 0);
+	return n;
+}
+int ReturnReveresNumber(int Number) {
+	int Number2 = 0;
+	int sum = 0;
+	while (Number>0) {
+		Number2=Number % 10;
+		Number = Number/10;
+		sum = sum * 10 + Number2;
+	}
+	return sum;
+		
+}
+int main()
+{
+    cout << ReturnReveresNumber(ReadPositivNumber("Please enter a positive number!\t"));
+	return 0;
+}
