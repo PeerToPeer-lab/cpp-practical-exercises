17b99ea7690b5498ece76b38dfb3a2b88db9689c
AboubakerDev <bouboualjzayr666@gmail.com>
2026-01-27 16:37:27 +0100

    Create 23 Fill Array with random Numbers .cpp

---
+++ Exercises/23 Fill Array with random Numbers .cpp
@@ -0,0 +1,48 @@
+/*
+Exercise 23
+Programming Advice - Course 5
+Solved in October 2024
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int ReadPositivNumberLess100(string Message) {
+	int n;
+	do {
+		cout << Message;
+		cin >> n;
+	} while (n <= 0 || n > 100);
+	return n;
+}
+
+int Random(int From, int To) {
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int Array[], int& ArrayLength) {
+	ArrayLength= ReadPositivNumberLess100("Please enter a number of elements\t");
+
+	for (int i =0; i < ArrayLength; i++)
+	{
+		Array[i] = Random(1,100);
+	}
+}
+
+void PrintArray(int ArrayElement[], int ArrayLength) {
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		cout << ArrayElement[i] << " ";
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Array[100];
+	int ArrauLength;
+	FillArrayWithRandomNumbers(Array, ArrauLength);
+	cout << "Array elemments are : \t";
+	PrintArray(Array, ArrauLength);
+	return 0;
+}
